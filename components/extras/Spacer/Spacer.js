90915605a602ee4dba250a36a0c991728c123320
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-08 06:54:58 -0700

    creates a 'Spacer' component

---
+++ components/extras/Spacer/Spacer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./Styles";
+
+const Spacer = () => {
+    return(
+        <View style={styles.spacerDiv}></View>
+    )
+}
+
+export default Spacer;
