82079e66a7e5a52eeac7f76cdc2a7df8babca2f0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-08 06:53:26 -0700

    creates styling for Home page

---
+++ components/Home/Styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'beige',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    greeting: {
+        textAlign: 'center'
+    },
+    startButton: {
+        backgroundColor: 'lightblue',
+        borderRadius: 10,
+        width: 300,
+    },
+    startButtonText: {
+        textAlign: 'center'
+    }
+});
+
+export default styles;
