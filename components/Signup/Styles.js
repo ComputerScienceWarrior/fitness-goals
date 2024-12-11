38163d11cfb0d8e927bb7fbb5efcd79172dc38a8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 09:30:10 -0700

    adds styling to text inputs and alert text

---
+++ components/Signup/Styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    header: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        padding: 5
+    },
+    textInputs: {
+        borderStyle: 'solid',
+        borderWidth: 2,
+        padding: 5,
+        margin: 5,
+        borderRadius: 10,
+    },
+    alertText: {
+        color: 'red'
+    },
+})
+
+export default styles;
