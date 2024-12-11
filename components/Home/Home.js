ca19ce988c3d426c174282349389601bf91152e0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 12:44:00 -0700

    creates button for Login navigation

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -9,8 +9,17 @@ const Home = ({ navigation }) => {
             <View>
                 <Text style={styles.greeting}>Reach your dream body, with Fitness Goals!</Text>
                 <Spacer />
-                <TouchableOpacity onPress={() => navigation.navigate('Signup')} style={styles.startButton}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Signup')} 
+                    style={styles.startButton}
+                >
                     <Text style={styles.startButtonText}>Start Here</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Login')}
+                    style={styles.loginButton}
+                >
+                    <Text style={styles.loginButtonText}>Login</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
