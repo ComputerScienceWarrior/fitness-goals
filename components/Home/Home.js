808574740aae185db9971cc1172618038c584f75
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 05:29:25 -0700

    adds in navigation capabilities to Home component

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,14 +3,14 @@ import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 import styles from "./Styles";
 import Spacer from "../extras/Spacer/Spacer";
 
-const Home = () => {
+const Home = ({ navigtion }) => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Reach your dream body, with Fitness Goals!</Text>
                 <Spacer />
                 <TouchableOpacity style={styles.startButton}>
-                    <Text style={styles.startButtonText}>Start Here</Text>
+                    <Text style={styles.startButtonText} onPress={() => navigtion.navigate('/signup')}>Start Here</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
