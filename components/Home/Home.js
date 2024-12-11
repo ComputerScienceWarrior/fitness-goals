df8d96d2dcf7b158fbf2d982cc9c1e05836210e1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 08:32:55 -0700

    fix navigation

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,14 +3,14 @@ import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 import styles from "./Styles";
 import Spacer from "../extras/Spacer/Spacer";
 
-const Home = ({ navigtion }) => {
+const Home = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Reach your dream body, with Fitness Goals!</Text>
                 <Spacer />
-                <TouchableOpacity style={styles.startButton}>
-                    <Text style={styles.startButtonText} onPress={() => navigtion.navigate('/signup')}>Start Here</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Signup')} style={styles.startButton}>
+                    <Text style={styles.startButtonText}>Start Here</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
