cf1e4c03c13efc1c42df8988e0cec3643f17b096
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-11 09:08:35 -0700

    adds in validations for fields

--- components/Signup/Signup.js
+++ components/Signup/Signup.js
@@ -1,21 +1,49 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, TouchableHighlight } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 const Signup = ({ navigation }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
     const [email, setEmail] = useState('');
 
     const userSignUp = () => {
-
-        console.log("validating User...")
-        navigation.navigate('Home')
+        if (username.length < 3) {
+            console.log('Username is too short')
+        } else if (password != passwordConfirmation){
+            console.log('Passwords do not match')
+        } else{
+            console.log(`validating User ${username}...Success!`)
+            navigation.navigate('Home')
+        }
     }
 
     return(
         <View>
             <Text>Sign Up</Text>
-            <TextInput placeholder="Create a username (between 4 - 15 characters)"/>
+            <TextInput 
+                placeholder="Create a username (between 4 - 15 characters)"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                secureTextEntry 
+                value={password}
+                placeholder="Enter a unique password"
+                onChangeText={setPassword}
+            />
+            <TextInput
+                secureTextEntry 
+                value={passwordConfirmation}
+                placeholder="Confirm password"
+                onChangeText={setPasswordConfirmation}
+            />
+            {password != passwordConfirmation ? <Text>Passwords do not match!</Text> : null }
+            <TextInput 
+                value={email}
+                placeholder="Enter Email Address"
+                onChangeText={setEmail}
+            />
             <TouchableOpacity onPress={() => userSignUp()}>
                 <Text>Create Account</Text>
             </TouchableOpacity>
