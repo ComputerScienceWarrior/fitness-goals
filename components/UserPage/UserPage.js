cd1106be2d44c24d944ceb56a1a0bea23e44cbc9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-13 06:14:44 -0700

    updates User Page temporary UI and adds in fetch for user data using fetchUserData fucntion and passed userId

--- components/UserPage/UserPage.js
+++ components/UserPage/UserPage.js
@@ -1,14 +1,35 @@
-import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, Alert } from "react-native";
+import fetchUserData from "../functions/fetchUserData";
 import User from "../User/User";
+import styles from "./Styles";
 
-const UserPage = ({ route }) => {
-    const { username } = route.params;  
+const UserPage = ({ route, navigation }) => {
+    const [userData, setUserData] = useState(null);
+    
+    useEffect(() => {
+        const loadUserData = async () => {
+            try {
+                const data = await fetchUserData(`users/${route.params["userId"]}`);
+                setUserData(data);
+            } catch (error) {
+                console.log(error)
+            }
+        };
 
-    return(
-        <View>
-            <Text>{username}'s fitness stats</Text>
-            <User username={username} />
+        loadUserData();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {userData ? (
+                <>
+                    <Text style={styles.header}>{route.params["username"]}'s Fitness Stats</Text>
+                    <User username={route.params["username"]} userData={userData} /> 
+                </>
+            ) : (
+                <Text style={styles.loading}>Loading...</Text>
+            )}
         </View>
     );
 };
