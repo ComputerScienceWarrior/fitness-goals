916a9d301170220e2a552c8c5ca4320bc0b07122
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-01 09:22:05 -0700

    creates a Stylesheet for the NewWorkoutForm component

---
+++ components/NewWorkout/Styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    newWorkoutFormWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '20px',
+        borderColor: 'black',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderRadius: 2
+    }
+});
+
+export default styles;
