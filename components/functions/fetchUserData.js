033e9ba80ccad561788860ec52949769c7f2ff95
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-01 16:50:43 -0700

    updates fetchUserData to grab the current user from the backend endpoint

--- components/functions/fetchUserData.js
+++ components/functions/fetchUserData.js
@@ -2,23 +2,26 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import { Alert } from 'react-native';
 
-const fetchUserData = async (endpoint) => {
+const fetchUserData = async (endpoint = "current_user") => {
+    
     try {
         const token = await SecureStore.getItemAsync('jwt');
         if (token) {
             const response = await axios.get(`http://192.168.12.175:3000/${endpoint}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
             });
-            console.log(`response data: ${response.data["username"]}`)
+            console.log(`Response data: ${JSON.stringify(response.data)}`);
             return response.data;
         } else {
             Alert.alert("Error", "No token found. Please log in again.");
+            throw new Error("No token found");
         }
     } catch (error) {
         console.error("Error fetching user data", error);
-        throw error; 
+        Alert.alert("Error", "Unable to fetch user data. Please try again.");
+        throw error;
     }
 };
 
