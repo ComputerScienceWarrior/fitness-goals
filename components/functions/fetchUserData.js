000f841d9421b267f80cddd6525db236845ff293
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-03 08:15:33 -0700

    fixes line spacing

--- components/functions/fetchUserData.js
+++ components/functions/fetchUserData.js
@@ -8,11 +8,10 @@ const fetchUserData = async (endpoint = "current_user") => {
         const token = await SecureStore.getItemAsync('jwt');
         if (token) {
             const response = await axios.get(`http://192.168.12.175:3000/${endpoint}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
             });
-            console.log(`Response data: ${JSON.stringify(response.data)}`);
             return response.data;
         } else {
             Alert.alert("Error", "No token found. Please log in again.");
