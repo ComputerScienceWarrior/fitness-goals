e42cdfbc2e346051521ae7ab68a5a397bdfef881
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-30 06:14:12 -0700

    checks for authentication and displays proper Drawer items

--- App.js
+++ App.js
@@ -4,14 +4,37 @@ import Login from './components/Login/Login';
 import UserPage from './components/UserPage/UserPage';
 import NewWorkoutForm from './components/NewWorkout/NewWorkoutForm';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useEffect, useState } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import LoadingScreen from './components/extras/Spacer/LoadingScreen';
 
-const Stack = createStackNavigator()
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await SecureStore.getItemAsync('jwt');
+      setIsAuthenticated(!!token);
+      setLoading(false);
+    };
+    checkAuth();
+  }, []);
+
+  if (loading) {
+    return (
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Loading" component={() => <LoadingScreen />} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
@@ -19,7 +42,9 @@ export default function App() {
         <Drawer.Screen name="Signup" component={Signup} />
         <Drawer.Screen name="Login" component={Login} />
         <Drawer.Screen name="NewWorkoutForm" component={NewWorkoutForm} />
-        <Drawer.Screen name="User Page" component={UserPage} />
+        {isAuthenticated && (
+          <Drawer.Screen name="User Page" component={UserPage} />
+        )}
       </Drawer.Navigator>
     </NavigationContainer>
   );
