9fe9c93217b3f1a2a6486945a813d00e4560cf5d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-02-06 09:45:16 -0700

    adds in request to query for a workout

--- components/WorkoutPage/WorkoutPage.js
+++ components/WorkoutPage/WorkoutPage.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { TextInput, TouchableOpacity, View, Text, SafeAreaView } from "react-native";
+import * as SecureStore from 'expo-secure-store';
+import { Alert } from "react-native";
+import axios from "axios";
 
 const WorkoutPage = ( {navigation, route }, props ) => {
     const [workoutId, setWorkoutId] = useState(null);
     const [workoutName, setWorkoutName] = useState("");
 
     useEffect(() => {
-        setWorkoutId(route.params.id);
-        setWorkoutName(route.params.name);
-    }, []);
+        const getWorkout = async () => {
+            try{
+                const token = SecureStore.getItemAsync('jwt');
+                if (token) {
+                    let woiD = route.params.id || route.params;
+                    const response = await axios.get(`http://192.168.12.175:3000/workouts/${woiD}/`)
+                    setWorkoutId(response.data.id);
+                    setWorkoutName(response.data.name);
+                } else{
+                    Alert('No token Present');
+                    navigation.navigate('Login');
+                }
+            } catch (error) {
+                Alert(error)
+            }
+        }
+        getWorkout();
+    }, [])
 
     return (
         <SafeAreaView>
